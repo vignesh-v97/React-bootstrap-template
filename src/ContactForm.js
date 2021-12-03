1c49439b09c9bd51a184e40efaa438a0baad7bc2
vignesh-v97 <vigneshbharathi34@gmail.com>
2021-12-03 10:18:23 +0530

    formik implemented

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,10 +1,43 @@
 import React from "react";
 import Divider from "./components/Divider";
+import {useFormik } from 'formik';
 
 function ContactForm() {
+  const formik = useFormik({
+    initialValues: {
+        name: '',
+        email: '',
+        phone: '',
+        message: ''
+    },
+    validate: values => {
+      const errors = {};
+
+      if(!values.name) {
+        errors.name = 'Required';
+      }
+
+      if(!values.email) {
+        errors.email = 'Required';
+      }
+
+      if(!values.phone) {
+        errors.phone = 'Required';
+      }
+
+      if(!values.message) {
+        errors.message = 'Required';
+      }
+
+      return errors;
+    },
+    onSubmit: values => {
+      console.log('🟢🟢🟢', values);
+    }
+  })
  return (
   <div>
-   <section className="page-section" id="contact">
+   <section className="page-section">
     <div className="container">
      <h2 className="mb-0 text-center page-section-heading text-uppercase text-secondary">
       Contact Me
@@ -12,33 +45,33 @@ function ContactForm() {
      <Divider />
      <div className="row justify-content-center">
       <div className="col-lg-8 col-xl-7">
-       <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+       <form onSubmit={formik.handleSubmit}>
         <div className="mb-3 form-floating">
          <input
           className="form-control"
           id="name"
+          name="name"
           type="text"
+          onChange={formik.handleChange}
+          value={formik.values.name}
           placeholder="Enter your name..."
-          data-sb-validations="required"
          />
          <label htmlFor="name">Full name</label>
-         <div className="invalid-feedback" data-sb-feedback="name:required">
-          A name is required.
-         </div>
+         {formik.errors.name ? <div className="">{formik.errors.name}</div> : null}
         </div>
         <div className="mb-3 form-floating">
          <input
           className="form-control"
           id="email"
           type="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
           placeholder="name@example.com"
-          data-sb-validations="required,email"
          />
          <label htmlFor="email">Email address</label>
-         <div className="invalid-feedback" data-sb-feedback="email:required">
-          An email is required.
-         </div>
-         <div className="invalid-feedback" data-sb-feedback="email:email">
+         {formik.errors.email ? <div className="">{formik.errors.email}</div> : null}
+         <div className="invalid-feedback">
           Email is not valid.
          </div>
         </div>
@@ -47,47 +80,36 @@ function ContactForm() {
           className="form-control"
           id="phone"
           type="tel"
+          name="phone"
+          onChange={formik.handleChange}
+          value={formik.values.phone}
           placeholder="(123) 456-7890"
-          data-sb-validations="required"
          />
          <label htmlFor="phone">Phone number</label>
-         <div className="invalid-feedback" data-sb-feedback="phone:required">
-          A phone number is required.
-         </div>
+         {formik.errors.phone ? <div className="">{formik.errors.phone}</div> : null}
         </div>
         <div className="mb-3 form-floating">
          <textarea
           className="form-control"
           id="message"
           type="text"
+          name="message"
+          onChange={formik.handleChange}
+          value={formik.values.message}
           placeholder="Enter your message here..."
           style={{ height: "10rem" }}
-          data-sb-validations="required"
           defaultValue={""}
          />
          <label htmlFor="message">Message</label>
-         <div className="invalid-feedback" data-sb-feedback="message:required">
-          A message is required.
-         </div>
+         {formik.errors.message ? <div className="">{formik.errors.message}</div> : null}
         </div>
-        <div className="d-none" id="submitSuccessMessage">
-         <div className="mb-3 text-center">
-          <div className="fw-bolder">Form submission successful!</div>
-          To activate this form, sign up at
-          <br />
-          <a href="https://startbootstrap.com/solution/contact-forms">
-           https://startbootstrap.com/solution/contact-forms
-          </a>
-         </div>
-        </div>
-        <div className="d-none" id="submitErrorMessage">
+        <div className="d-none">
          <div className="mb-3 text-center text-danger">
           Error sending message!
          </div>
         </div>
         <button
-         className="btn btn-primary btn-xl disabled"
-         id="submitButton"
+         className="btn btn-primary btn-xl"
          type="submit"
         >
          Send
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,30 +1,46 @@
-import React from 'react';
+import React from "react";
 
 const navTitles = ["portfolio", "about", "contact"];
 
 function Navbar() {
  return (
   <div>
-   <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-          <div className="container">
-            <a className="navbar-brand" href="/">Start Bootstrap</a>
-            <button className="text-white rounded navbar-toggler text-uppercase font-weight-bold bg-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-              Menu
-              <i className="fas fa-bars" />
-            </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-              <ul className="navbar-nav ms-auto">
-                {
-                  navTitles.map((title) => {
-                    return <li className="mx-0 nav-item mx-lg-1"><a className="px-0 py-3 rounded nav-link px-lg-3" href="#portfolio">{title}</a></li>
-                  })
-                }
-              </ul>
-            </div>
-          </div>
-    </nav>
+   <nav
+    className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top"
+    id="mainNav"
+   >
+    <div className="container">
+     <a className="navbar-brand" href="/">
+      Start Bootstrap
+     </a>
+     <button
+      className="text-white rounded navbar-toggler text-uppercase font-weight-bold bg-primary"
+      type="button"
+      data-bs-target="#navbarResponsive"
+      aria-controls="navbarResponsive"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+     >
+      Menu
+      <i className="fas fa-bars" />
+     </button>
+     <div className="collapse navbar-collapse" id="navbarResponsive">
+      <ul className="navbar-nav ms-auto">
+       {navTitles.map((title, index) => {
+        return (
+         <li key={index} className="mx-0 nav-item mx-lg-1">
+          <a className="px-0 py-3 rounded nav-link px-lg-3" href="#portfolio">
+           {title}
+          </a>
+         </li>
+        );
+       })}
+      </ul>
+     </div>
+    </div>
+   </nav>
   </div>
- )
+ );
 }
 
-export default Navbar
+export default Navbar;
