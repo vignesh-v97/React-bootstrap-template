91daab1467ad24d8c91272a56f889a6198fd2f9e
vignesh-v97 <vigneshbharathi34@gmail.com>
2021-12-04 09:26:37 +0530

    formik implemented

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,40 +1,9 @@
 import React from "react";
 import Divider from "./components/Divider";
-import {useFormik } from 'formik';
+import { Formik } from "formik";
+import * as Yup from 'yup';
 
 function ContactForm() {
-  const formik = useFormik({
-    initialValues: {
-        name: '',
-        email: '',
-        phone: '',
-        message: ''
-    },
-    validate: values => {
-      const errors = {};
-
-      if(!values.name) {
-        errors.name = 'Required';
-      }
-
-      if(!values.email) {
-        errors.email = 'Required';
-      }
-
-      if(!values.phone) {
-        errors.phone = 'Required';
-      }
-
-      if(!values.message) {
-        errors.message = 'Required';
-      }
-
-      return errors;
-    },
-    onSubmit: values => {
-      console.log('🟢🟢🟢', values);
-    }
-  })
  return (
   <div>
    <section className="page-section">
@@ -45,76 +14,106 @@ function ContactForm() {
      <Divider />
      <div className="row justify-content-center">
       <div className="col-lg-8 col-xl-7">
-       <form onSubmit={formik.handleSubmit}>
-        <div className="mb-3 form-floating">
-         <input
-          className="form-control"
-          id="name"
-          name="name"
-          type="text"
-          onChange={formik.handleChange}
-          value={formik.values.name}
-          placeholder="Enter your name..."
-         />
-         <label htmlFor="name">Full name</label>
-         {formik.errors.name ? <div className="">{formik.errors.name}</div> : null}
-        </div>
-        <div className="mb-3 form-floating">
-         <input
-          className="form-control"
-          id="email"
-          type="email"
-          name="email"
-          onChange={formik.handleChange}
-          value={formik.values.email}
-          placeholder="name@example.com"
-         />
-         <label htmlFor="email">Email address</label>
-         {formik.errors.email ? <div className="">{formik.errors.email}</div> : null}
-         <div className="invalid-feedback">
-          Email is not valid.
-         </div>
-        </div>
-        <div className="mb-3 form-floating">
-         <input
-          className="form-control"
-          id="phone"
-          type="tel"
-          name="phone"
-          onChange={formik.handleChange}
-          value={formik.values.phone}
-          placeholder="(123) 456-7890"
-         />
-         <label htmlFor="phone">Phone number</label>
-         {formik.errors.phone ? <div className="">{formik.errors.phone}</div> : null}
-        </div>
-        <div className="mb-3 form-floating">
-         <textarea
-          className="form-control"
-          id="message"
-          type="text"
-          name="message"
-          onChange={formik.handleChange}
-          value={formik.values.message}
-          placeholder="Enter your message here..."
-          style={{ height: "10rem" }}
-          defaultValue={""}
-         />
-         <label htmlFor="message">Message</label>
-         {formik.errors.message ? <div className="">{formik.errors.message}</div> : null}
-        </div>
-        <div className="d-none">
-         <div className="mb-3 text-center text-danger">
-          Error sending message!
-         </div>
-        </div>
-        <button
-         className="btn btn-primary btn-xl"
-         type="submit"
-        >
-         Send
-        </button>
-       </form>
+       <Formik
+        initialValues={{
+         name: "",
+         email: "",
+         phone: "",
+         message: "",
+        }}
+        validate={(values) => {
+         const errors = {};
+
+         if (!values.name) {
+          errors.name = "Required";
+         }
+
+         if (!values.email) {
+          errors.email = "Required";
+         }
+
+         if (!values.phone) {
+          errors.phone = "Required";
+         }
+
+         if (!values.message) {
+          errors.message = "Required";
+         }
+
+         return errors;
+        }}
+        validationSchema = { Yup.object({
+          email: Yup.string().email('Invalid Email Adress').required('Required')
+        })}
+        onSubmit={(values) => {
+         console.log("🟢🟢🟢", values);
+        }}
+       >
+        {({ values, errors, touched, handleChange, handleSubmit, handleBlur, getFieldProps }) => (
+         <form onSubmit={handleSubmit}>
+          <div className="mb-3 form-floating">
+           <input
+            className="form-control"
+            id="name"
+            name="name"
+            type="text"
+           {...getFieldProps('name')}
+            placeholder="Enter your name..."
+           />
+           <label htmlFor="name">Full name</label>
+           {touched.name && errors.name  ? (<div className="error">{errors.name}</div>) : null}
+          </div>
+          <div className="mb-3 form-floating">
+           <input
+            className="form-control"
+            id="email"
+            type="email"
+            name="email"
+            {...getFieldProps('email')}
+            placeholder="name@example.com"
+           />
+           <label htmlFor="email">Email address</label>
+           {errors.email && touched.email ? <div className="error">{errors.email}</div> : null}
+           <div className="invalid-feedback">Email is not valid.</div>
+          </div>
+          <div className="mb-3 form-floating">
+           <input
+            className="form-control"
+            id="phone"
+            type="tel"
+            name="phone"
+            {...getFieldProps('phone')}
+            placeholder="(123) 456-7890"
+           />
+           <label htmlFor="phone">Phone number</label>
+           {errors.phone && touched.phone ? <div className="error">{errors.phone}</div> : null}
+          </div>
+          <div className="mb-3 form-floating">
+           <textarea
+            className="form-control"
+            id="message"
+            type="text"
+            name="message"
+            {...getFieldProps('message')}
+            placeholder="Enter your message here..."
+            style={{ height: "10rem" }}
+           />
+           <label htmlFor="message">Message</label>
+           {errors.message && touched.message ? (
+            <div className="error">{errors.message}</div>
+           ) : null}
+          </div>
+          <div className="d-none">
+           <div className="mb-3 text-center text-danger">
+            Error sending message!
+           </div>
+          </div>
+          <button className="btn btn-primary btn-xl" type="submit">
+           Send
+          </button>
+         </form>
+        )}
+       </Formik>
       </div>
      </div>
     </div>
